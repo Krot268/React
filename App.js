2583923756f76716cfa538b9f4b44760fdc00dc9
Krot268 <103069422+Krot268@users.noreply.github.com>
2022-12-15 20:33:08 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react'
+
+import { Form } from './components/Form/Form'
+import { MessageList } from './components/MessageList/MessageList'
+
+import { AUTHOR } from './constants'
+
+
+export function App() {
+  const [messages, setMessages] = useState([])
+
+  const addMessage = (newMessage) => {
+    console.log('newMessage', newMessage);
+    setMessages([...messages, newMessage])
+  }
+
+  useEffect(() => {
+    if (messages.length > 0 && messages[messages.length - 1].author === AUTHOR.user) {
+      const timeout = setTimeout(() => {
+        addMessage({
+          author: AUTHOR.bot,
+          text: 'Im BOT'
+        })
+      }, 1500)
+
+      return () => {
+        clearTimeout(timeout)
+      }
+    }
+  }, //[messages]
+  )
+
+
+  return (
+    <>
+      <h1>Welcome to chat!</h1>
+      <hr></hr>
+      <Form addMessage={addMessage} />
+      <MessageList messages={messages} />
+    </>
+  )
+}
+
+// export default App
